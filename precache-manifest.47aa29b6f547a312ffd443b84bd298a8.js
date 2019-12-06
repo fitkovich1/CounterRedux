75b3411261053e456cc06eb65f0390be5222c38f
fitkovich1 <fitkovich1@gmai.com>
2019-12-06 16:58:36 +0300

    Updates

---
+++ precache-manifest.47aa29b6f547a312ffd443b84bd298a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d702d18e6609d1fdb2c13f33f2aac92",
+    "url": "/CounterRedux/index.html"
+  },
+  {
+    "revision": "c0a3d7584c0d2767ba2a",
+    "url": "/CounterRedux/static/css/main.407eac69.chunk.css"
+  },
+  {
+    "revision": "028a0afdb0f1f27e3228",
+    "url": "/CounterRedux/static/js/2.a2b469ac.chunk.js"
+  },
+  {
+    "revision": "c0a3d7584c0d2767ba2a",
+    "url": "/CounterRedux/static/js/main.a54077bd.chunk.js"
+  },
+  {
+    "revision": "0286ba39e535d5c60df3",
+    "url": "/CounterRedux/static/js/runtime-main.70a0c4eb.js"
+  }
+]);
